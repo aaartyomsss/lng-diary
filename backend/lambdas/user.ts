07bb990773b541111cf96549a4f103c80a555652
Artjom Savin <aaartyomsss@gmail.com>
2024-12-25 14:09:11 +0200

    Implement and test user creation endpoint

---
+++ backend/lambdas/user.ts
@@ -0,0 +1,66 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+
+const postUserPayloadSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8).max(32),
+    confirmPassword: z.string().min(8).max(32),
+});
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Invalid body',
+            }),
+        };
+    }
+    const body = JSON.parse(event.body);
+    const parsedBody = postUserPayloadSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Invalid body',
+            }),
+        };
+    }
+
+    const { email, password, confirmPassword } = parsedBody.data;
+
+    if (confirmPassword !== password) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Passwords do not match',
+            }),
+        };
+    }
+
+    await createUser(email, password);
+
+    try {
+        return {
+            statusCode: 201,
+            body: JSON.stringify({
+                message: 'User created',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+
+const createUser = async (email: string, password: string) => {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    // TODO: To add DB logic
+};
