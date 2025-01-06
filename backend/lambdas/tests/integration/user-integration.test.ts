bf86bb63474436d1fbae526c35bacbe90799e6b1
Artjom Savin <aaartyomsss@gmail.com>
2025-01-06 11:24:19 +0200

    Implement base environment for integration tests

---
+++ backend/lambdas/tests/integration/user-integration.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, it, jest } from '@jest/globals';
+
+describe('Testing base user DB functionality', () => {
+    it('Test retreival of User', () => {
+        expect(1).toBe(1);
+    });
+});
