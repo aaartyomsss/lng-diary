baefec5b0963ac4103f34dbe9c2b46da5ce89dbf
Artjom Savin <aaartyomsss@gmail.com>
2025-02-25 21:19:54 +0200

    Add base LLM Service class

--- backend/lambdas/apiTest.ts
+++ backend/lambdas/apiTest.ts
@@ -1,15 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { generateJwt } from './utils/authorization';
+import { LlmServcice } from './services/llmService';
 
 async function main() {
-  const token = generateJwt(2);
-
-  console.log(token);
+  const llm = new LlmServcice();
+  const res = await llm.makeTranslationRequest('Text');
+  res.choices.forEach((c) => {
+    console.log(c.message);
+  });
+  console.log(res.choices);
 }
 
 main()
-  .then((res) => {
-    console.log(res);
+  .then((_) => {
+    console.log('Done');
   })
   .catch((e) => console.error(e));
---
+++ backend/lambdas/services/LlmService.ts
@@ -0,0 +1,40 @@
+import OpenAI from 'openai';
+
+export class LlmServcice {
+  #client;
+
+  #BASE_PROMPT = `
+    I am going you to send some text. Text is going to be mainly in Finnish. However,
+    some words are going to be substituted with English. Your job is to correct the Finnish
+    text, substitute the English word with Finnish. Lastly create a JSON out of filler English words 
+    in base form for both languages. Return format should be strictly this:
+    
+    Corrected text: <Corrected_text> | JSON with translations: <JSON>
+
+
+    Here is the text you should translate: Se on maybe tosi pitkä sentence ja mä en tiedä can I kirtjoitteta it all itse
+  `;
+
+  constructor() {
+    if (!process.env.EXTERNAL_LLM_API_KEY) {
+      throw new Error('External API key for LLM is not set');
+    }
+    this.#client = new OpenAI({
+      apiKey: process.env.EXTERNAL_LLM_API_KEY,
+    });
+  }
+
+  async makeTranslationRequest(_userInput: string) {
+    const chat = await this.#client.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content: this.#BASE_PROMPT,
+        },
+      ],
+      model: 'gpt-4o-mini',
+    });
+
+    return chat;
+  }
+}
